058aaa0b1ce81ad7feb5005e38f1e4359115335d
Laura <laura.hun03@gmail.com>
2025-02-25 15:41:28 +0100

    page accueil

---
+++ app/vues/Page_accueil.php
@@ -0,0 +1,62 @@
+<h1>Centre aéré et périscolaire Les Bambins</h1>
+<div id="banderole"><img src="images/Header2.png" alt="Header" ></div>
+<div id="text_container"><p>Nous sommes heureux de vous accueillir sur le site du centre aéré "Les Bambins", un lieu d'épanouissement, de partage
+    et d'apprentissage pour vos enfants. Notre centre aéré a pour vocation de proposer un cadre sécurisé, bienveillant
+    et stimulant, où chaque enfant peut s'épanouir pleinement tout en découvrant de nouvelles expériences.
+
+    Depuis sa création, le centre "Les Bambins" s'engage à offrir des activités enrichissantes qui favorisent le
+    développement personnel, social et créatif de chaque enfant. Nos équipes sont composées d'animateurs formés et
+    passionnés, soucieux de veiller à la sécurité et au bien-être des enfants tout en leur offrant des moments de
+    plaisir et d'apprentissage.<br><br>
+
+    Nous croyons fermement que l'enfance est une période essentielle où les enfants doivent pouvoir explorer, grandir et
+    tisser des liens dans un environnement à la fois ludique et éducatif. C’est pourquoi nous avons conçu un programme
+    d’activités varié et adapté à chaque tranche d’âge, permettant à chacun de s’épanouir à son propre rythme. Des
+    activités sportives aux ateliers créatifs, en passant par des moments de découverte en plein air, chaque journée est
+    une nouvelle occasion pour vos enfants d’apprendre tout en s'amusant.
+
+    Notre engagement va au-delà des simples loisirs : nous souhaitons contribuer activement à l'éducation de vos enfants
+    en leur offrant des expériences qui nourrissent leur curiosité et leur créativité. En tant que parents, vous pouvez
+    avoir l’esprit tranquille en sachant que vos enfants évoluent dans un environnement respectueux de leurs besoins, où
+    l’écoute, la bienveillance et la convivialité sont au cœur de nos valeurs.<br><br></p></div>
+<a href="index.php?page=register"><input type="button" class="bouton" value="Inscrire mon enfant"></a>
+<h2>Où nous trouver ?</h2>
+<div class="maps">
+    <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5568.238639080962!2d4.857885876008181!3d45.748753814408175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ea718d70402d%3A0x83b9f0d84170407f!2sIAE%20Lyon%20School%20Of%20Management!5e0!3m2!1sen!2sfr!4v1738946695714!5m2!1sen!2sfr"
+                allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <div><p>Le centre aéré "Les Bambins" est idéalement situé au cœur du quartier universitaire de la Manufacture des
+        Tabacs, à Lyon, facilement accessible pour les familles des environs. Vous pouvez visualiser notre emplacement
+        exact sur la carte ci-dessous, ce qui vous aidera à mieux planifier votre visite.<br><br>
+
+        Notre localisation est pratique, avec des transports en commun à proximité (tramway, métro et bus) et un
+        environnement calme et sécurisé, parfait pour accueillir vos enfants dans les meilleures conditions.<br><br>
+
+        N'hésitez pas à nous contacter si vous avez besoin d'indications supplémentaires pour nous rejoindre !
+    </p>
+    </div>
+</div>
+<h2>Nos certifications</h2>
+<div class="certif">
+    <div><p>Au centre aéré "Les Bambins", nous sommes fiers d’offrir un environnement sûr, enrichissant et respectueux
+        des
+        besoins de chaque enfant. Nous avons obtenu plusieurs certifications, dont la Certification Qualité Accueil
+        Enfants
+        et l'Agrément Jeunesse et Sports, qui témoignent de notre engagement à fournir des services de qualité et
+        conformes
+        aux normes les plus strictes.<br><br>
+
+        Ces certifications assurent que nos installations, nos pratiques et nos activités respectent les exigences
+        légales
+        et pédagogiques en matière de sécurité, d'hygiène et de bien-être. Elles reflètent également notre volonté
+        constante
+        d’améliorer nos services pour garantir une expérience optimale aux enfants.<br><br>
+
+        Notre équipe est régulièrement formée pour s'assurer que chaque enfant bénéficie d'un accueil personnalisé et
+        d'activités adaptées à ses besoins. Grâce à ces certifications, nous continuons à offrir un cadre sécurisé,
+        ludique
+        et éducatif, tout en veillant à l’épanouissement de chaque enfant.</p></div>
+    <div id="certif_img"><img src="images/certif.png"></div>
+</div>

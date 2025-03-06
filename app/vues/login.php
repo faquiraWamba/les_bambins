0d6cb5927ad8ab5e1aa136b5847d1aba58474203
Laura <laura.hun03@gmail.com>
2025-03-06 12:18:00 +0100

    page de connexion

---
+++ app/vues/login.php
@@ -0,0 +1,13 @@
+<h1 class="luckiest-guy" id="connexion">Connexion</h1>
+<form>
+    <fieldset>
+        <label for="id" class="connexion">Identifiant</label>
+        <input type="text" id="id" class="connexion" name="id" placeholder="Entrez votre identifiant">
+        <label for="mdp" class="connexion">Mot de passe</label>
+        <input type="password" id="mdp" class="connexion" name="mdp" placeholder="Entrer votre Mot de passe"><br>
+        <input type="checkbox" id="enregistrer" class="connexion"/>
+        <label for="enregistrer" class="checkbox">Se souvenir de moi</label>
+        <button type="submit" class="bouton">Connexion</button>
+        <a href="inscription">Pas encore de compte?</a>
+    </fieldset>
+</form>

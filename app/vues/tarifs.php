ec02d38a2823a0083f8412a05bd05bc19888055e
fatou <lomamefatou0@gmail.com>
2025-03-12 12:04:56 +0100

    tarif

---
+++ app/vues/tarifs.php
@@ -0,0 +1,72 @@
+
+
+    <section class="tarifs">
+        <h1>Nos tarifs</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Catégorie</th>
+                    <th>Durée</th>
+                    <th>Prix</th>
+                    <th>Détail</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td rowspan="3" class="category">PÉRISCOLAIRE</td>
+                    <td>Matin</td>
+                    <td>xx€/matin</td>
+                    <td>De xH à xH</td>
+                </tr>
+                <tr>
+                    <td>Soir</td>
+                    <td>xx€/soir</td>
+                    <td>De xH à xH</td>
+                </tr>
+                <tr>
+                    <td>Matin + Soir</td>
+                    <td>xx€</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td rowspan="2" class="category">MERCREDI</td>
+                    <td>Demi-journée</td>
+                    <td>xx€</td>
+                    <td>Avec ou sans repas</td>
+                </tr>
+                <tr>
+                    <td>Journée complète</td>
+                    <td>xx€</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td rowspan="2" class="category">VACANCES SCOLAIRES</td>
+                    <td>1 semaine</td>
+                    <td>xx€</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Vacances complète</td>
+                    <td>xx€</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="category">REPAS</td>
+                    <td>30 minutes?</td>
+                    <td>xx€</td>
+                    <td>Repas équilibrés, adaptés aux régimes spéciaux</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+
+    <section class="infos">
+        <h2>Informations importantes</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius, turpis et commodo pharetra..Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma, ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi, humanitatis multiformibus officiis retentabant..</p>
+    </section>
+
+    <section class="inscription">
+        <h2>Intéressé? Inscrivez votre enfant</h2>
+        <a href="#" class="btn-inscription">INSCRIVEZ VOTRE ENFANT</a>
+    </section>
+
